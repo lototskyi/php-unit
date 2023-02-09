cc61116bc1f645998b088a4559ec297721a01d5a
alex <o.lototskyi@gmail.com>
2023-02-09 16:56:16 +0200

    class autoload and PHPUnit configuration

--- public/tests/UserTest.php
+++ public/tests/UserTest.php
@@ -1,14 +1,12 @@
 <?php
 
-use \PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\TestCase;
 
 class UserTest extends TestCase
 {
 
     public function testReturnsFullName()
     {
-        require "User.php";
-
         $user = new User;
 
         $user->first_name = "Teresa";
